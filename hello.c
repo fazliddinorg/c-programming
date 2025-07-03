016652bf6cdfcac31ba28af436fac60b10ed7e4c
Fazliddin Mamutkhanov <103223186+FazliddinOrg@users.noreply.github.com>
2025-07-03 23:31:36 +0900

    hello world

---
+++ hello.c
@@ -0,0 +1,16 @@
+/* 
+#include <stdio.h> tells the compiler to include the Standard Input Output library. This is necessary for using printf, which outputs text.
+
+int main(void) is the entry point of every C program. The code inside this block is executed first.
+
+return 0; signifies that the program has finished successfully.
+ */
+
+
+ #include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, world!\n");
+    return 0;
+}
