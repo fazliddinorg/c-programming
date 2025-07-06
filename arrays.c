ed58619de6a1c9c97a37a9f1f77abfe068e31ef6
Fazliddin Mamutkhanov <103223186+FazliddinOrg@users.noreply.github.com>
2025-07-06 20:15:01 +0900

    Create arrays.c

---
+++ arrays.c
@@ -0,0 +1,13 @@
+/*
+Arrays store multiple values of the same type.
+Access using index, starting at 0.
+*/
+
+int main()
+{
+    int nums[3] = {10, 20, 30};
+    printf("First: %d\n", nums[0]);
+    nums[1] = 25;
+    printf("Second: %d\n", nums[1]);
+    return 0;
+}
