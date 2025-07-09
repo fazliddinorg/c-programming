05db2c671d8352a0f2d3ae09d99225e096c7ec11
Fazliddin Mamutkhanov <103223186+fazliddinorg@users.noreply.github.com>
2025-07-09 14:09:27 +0900

    strings.c

---
+++ strings.c
@@ -0,0 +1,11 @@
+// Strings are arrays of characters ending with '\0'.
+// Use %s to print strings.
+
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Fazliddin";
+    printf("Hello, %s!\n", name);
+    return 0;
+}
