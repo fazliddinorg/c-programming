5f8dfa2c6b19d206caa7c48e9aecf2e1fabb257c
Fazliddin Mamutkhanov <103223186+fazliddinorg@users.noreply.github.com>
2025-07-16 17:54:57 +0900

    file_io.c

--- file_io.c
+++ file_io.c
@@ -1,1 +1,22 @@
+/*
+You can read/write files using FILE* and fopen(), fclose(), fscanf(), fprintf().
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *file = fopen("data.txt", "w");
+
+    if (file == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    fprintf(file, "Hello, file!\n");
+    fclose(file);
+
+    return 0;
+}
 
