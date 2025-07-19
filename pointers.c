76641e1ef770877d36cc517e717574f70d17864d
Fazliddin Mamutkhanov <103223186+fazliddinorg@users.noreply.github.com>
2025-07-19 18:30:24 +0900

    pointers.c

--- pointers.c
+++ pointers.c
@@ -1,16 +1,31 @@
 // Pointers store memory addresses.
-// Use * to declare, & to get address.
+// * declares a pointer, & gets the address of a variable.
 
 #include <stdio.h>
 
-int main()
+int main(void)
 {
+    // Declare an integer variable
     int x = 10;
-    int *ptr = &x;
 
+    // Declare a pointer to an int, and store the address of x
+    int *p = &x;
+
+    // Print the value of x directly
     printf("Value of x: %d\n", x);
+
+    // Print the address of x using &
     printf("Address of x: %p\n", &x);
-    printf("Value using pointer: %d\n", *ptr);
+
+    // Print the address stored in pointer p
+    printf("Pointer p points to: %p\n", p);
+
+    // Dereference the pointer to access the value at that address
+    printf("Value at address p points to: %d\n", *p);
+
+    // Change x using the pointer
+    *p = 20;
+    printf("New value of x after *p = 20: %d\n", x);
 
     return 0;
 }
